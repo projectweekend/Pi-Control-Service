3f848c4f485bb95ef8ef7a8829438ef6654592d9
Brian Hines <brian@projectweekend.net>
2015-01-02 17:46:29 -0600

    Fix README.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pi-Control-Service',
-    version='0.0.1',
+    version='0.0.2',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pi_control'],
