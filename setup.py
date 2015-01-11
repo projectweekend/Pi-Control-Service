b5617ad0256c3e2b25ee2a25af3cf6e672ddb99d
Brian Hines <brian@projectweekend.net>
2015-01-10 20:43:37 -0600

    Publish pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pi-Control-Service',
-    version='0.0.10',
+    version='0.1.0',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pi_control_service'],
