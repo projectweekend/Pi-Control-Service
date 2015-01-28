6cafab27b92364546bc6163e3850705136f1e633
Brian Hines <brian@projectweekend.net>
2015-01-27 20:27:05 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pi-Control-Service',
-    version='0.1.0',
+    version='0.1.1',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pi_control_service'],
